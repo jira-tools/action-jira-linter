dee665c97654e991426fd133d1833fc42aac5d5b
Asbjørn Ulsberg <asbjorn@ulsberg.no>
2022-07-28 14:27:54 +0200

    Run ESLint on tests
    Run ESLint on tests and fix all issues discovered in them.

--- __tests__/github.test.ts
+++ __tests__/github.test.ts
@@ -1,117 +1,75 @@
-import { GitHub } from "../src/github";
-import { HIDDEN_MARKER } from "../src/constants";
+/* eslint-disable filenames/match-regex */
 
-jest.spyOn(console, "log").mockImplementation(); // avoid actual console.log in test output
+import { GitHub } from '../src/github';
+import { HIDDEN_MARKER } from '../src/constants';
 
-describe("shouldSkipBranchLint()", () => {
-  it("should recognize bot PRs", () => {
-    expect(GitHub.shouldSkipBranchLint("dependabot")).toBe(true);
-    expect(
-      GitHub.shouldSkipBranchLint(
-        "dependabot/npm_and_yarn/types/react-dom-16.9.6",
-      ),
-    ).toBe(true);
-    expect(GitHub.shouldSkipBranchLint("feature/add-dependabot-config")).toBe(
-      false,
-    );
-    expect(GitHub.shouldSkipBranchLint("feature/add-dependabot-config-OSS-101"))
-      .toBe(false);
+jest.spyOn(console, 'log').mockImplementation(); // avoid actual console.log in test output
 
-    expect(GitHub.shouldSkipBranchLint("all-contributors")).toBe(true);
-    expect(GitHub.shouldSkipBranchLint("all-contributors/add-ghost")).toBe(
-      true,
-    );
-    expect(GitHub.shouldSkipBranchLint("chore/add-all-contributors")).toBe(
-      false,
-    );
-    expect(GitHub.shouldSkipBranchLint("chore/add-all-contributors-OSS-102"))
-      .toBe(false);
+describe('shouldSkipBranchLint()', () => {
+  it('should recognize bot PRs', () => {
+    expect(GitHub.shouldSkipBranchLint('dependabot')).toBe(true);
+    expect(GitHub.shouldSkipBranchLint('dependabot/npm_and_yarn/types/react-dom-16.9.6')).toBe(true);
+    expect(GitHub.shouldSkipBranchLint('feature/add-dependabot-config')).toBe(false);
+    expect(GitHub.shouldSkipBranchLint('feature/add-dependabot-config-OSS-101')).toBe(false);
+
+    expect(GitHub.shouldSkipBranchLint('all-contributors')).toBe(true);
+    expect(GitHub.shouldSkipBranchLint('all-contributors/add-ghost')).toBe(true);
+    expect(GitHub.shouldSkipBranchLint('chore/add-all-contributors')).toBe(false);
+    expect(GitHub.shouldSkipBranchLint('chore/add-all-contributors-OSS-102')).toBe(false);
   });
 
-  it("should handle custom ignore patterns", () => {
-    expect(GitHub.shouldSkipBranchLint("bar", "^bar")).toBeTruthy();
-    expect(GitHub.shouldSkipBranchLint("foobar", "^bar")).toBeFalsy();
+  it('should handle custom ignore patterns', () => {
+    expect(GitHub.shouldSkipBranchLint('bar', '^bar')).toBeTruthy();
+    expect(GitHub.shouldSkipBranchLint('foobar', '^bar')).toBeFalsy();
 
-    expect(GitHub.shouldSkipBranchLint("bar", "[0-9]{2}")).toBeFalsy();
-    expect(GitHub.shouldSkipBranchLint("bar", "")).toBeFalsy();
-    expect(GitHub.shouldSkipBranchLint("foo", "[0-9]{2}")).toBeFalsy();
-    expect(GitHub.shouldSkipBranchLint("f00", "[0-9]{2}")).toBeTruthy();
+    expect(GitHub.shouldSkipBranchLint('bar', '[0-9]{2}')).toBeFalsy();
+    expect(GitHub.shouldSkipBranchLint('bar', '')).toBeFalsy();
+    expect(GitHub.shouldSkipBranchLint('foo', '[0-9]{2}')).toBeFalsy();
+    expect(GitHub.shouldSkipBranchLint('f00', '[0-9]{2}')).toBeTruthy();
 
-    const customBranchRegex = "^(production-release|master|release/v\\d+)$";
-    expect(GitHub.shouldSkipBranchLint("production-release", customBranchRegex))
-      .toBeTruthy();
-    expect(GitHub.shouldSkipBranchLint("master", customBranchRegex))
-      .toBeTruthy();
-    expect(GitHub.shouldSkipBranchLint("release/v77", customBranchRegex))
-      .toBeTruthy();
+    const customBranchRegex = '^(production-release|master|release/v\\d+)$';
+    expect(GitHub.shouldSkipBranchLint('production-release', customBranchRegex)).toBeTruthy();
+    expect(GitHub.shouldSkipBranchLint('master', customBranchRegex)).toBeTruthy();
+    expect(GitHub.shouldSkipBranchLint('release/v77', customBranchRegex)).toBeTruthy();
 
-    expect(
-      GitHub.shouldSkipBranchLint(
-        "release/very-important-feature",
-        customBranchRegex,
-      ),
-    ).toBeFalsy();
-    expect(GitHub.shouldSkipBranchLint("masterful", customBranchRegex))
-      .toBeFalsy();
-    expect(GitHub.shouldSkipBranchLint("productionish", customBranchRegex))
-      .toBeFalsy();
-    expect(
-      GitHub.shouldSkipBranchLint("fix/production-issue", customBranchRegex),
-    ).toBeFalsy();
-    expect(
-      GitHub.shouldSkipBranchLint(
-        "chore/rebase-with-master",
-        customBranchRegex,
-      ),
-    ).toBeFalsy();
-    expect(
-      GitHub.shouldSkipBranchLint(
-        "chore/rebase-with-release",
-        customBranchRegex,
-      ),
-    ).toBeFalsy();
-    expect(
-      GitHub.shouldSkipBranchLint(
-        "chore/rebase-with-release/v77",
-        customBranchRegex,
-      ),
-    ).toBeFalsy();
+    expect(GitHub.shouldSkipBranchLint('release/very-important-feature', customBranchRegex)).toBeFalsy();
+    expect(GitHub.shouldSkipBranchLint('masterful', customBranchRegex)).toBeFalsy();
+    expect(GitHub.shouldSkipBranchLint('productionish', customBranchRegex)).toBeFalsy();
+    expect(GitHub.shouldSkipBranchLint('fix/production-issue', customBranchRegex)).toBeFalsy();
+    expect(GitHub.shouldSkipBranchLint('chore/rebase-with-master', customBranchRegex)).toBeFalsy();
+    expect(GitHub.shouldSkipBranchLint('chore/rebase-with-release', customBranchRegex)).toBeFalsy();
+    expect(GitHub.shouldSkipBranchLint('chore/rebase-with-release/v77', customBranchRegex)).toBeFalsy();
   });
 
-  it("should return false with empty input", () => {
-    expect(GitHub.shouldSkipBranchLint("")).toBeFalsy();
+  it('should return false with empty input', () => {
+    expect(GitHub.shouldSkipBranchLint('')).toBeFalsy();
   });
 
-  it("should return false for other branches", () => {
-    expect(GitHub.shouldSkipBranchLint("feature/awesomeNewFeature"))
-      .toBeFalsy();
+  it('should return false for other branches', () => {
+    expect(GitHub.shouldSkipBranchLint('feature/awesomeNewFeature')).toBeFalsy();
   });
 });
 
-describe("getHotFixLabel()", () => {
-  it("should return empty string for master branch", () => {
-    expect(GitHub.getHotfixLabel("master")).toEqual("");
+describe('getHotFixLabel()', () => {
+  it('should return empty string for master branch', () => {
+    expect(GitHub.getHotfixLabel('master')).toEqual('');
   });
 
-  it("should return HOTFIX-PROD for production branch", () => {
-    expect(GitHub.getHotfixLabel("production-release")).toEqual(
-      GitHub.LABELS.HOTFIX_PROD,
-    );
+  it('should return HOTFIX-PROD for production branch', () => {
+    expect(GitHub.getHotfixLabel('production-release')).toEqual(GitHub.labels.hotfixProd);
   });
 
-  it("should return HOTFIX-PRE-PROD for release branch", () => {
-    expect(GitHub.getHotfixLabel("release/v")).toEqual(
-      GitHub.LABELS.HOTFIX_PRE_PROD,
-    );
+  it('should return HOTFIX-PRE-PROD for release branch', () => {
+    expect(GitHub.getHotfixLabel('release/v')).toEqual(GitHub.labels.hotfixPreProd);
   });
 
-  it("should return empty string with no input", () => {
-    expect(GitHub.getHotfixLabel("")).toEqual("");
+  it('should return empty string with no input', () => {
+    expect(GitHub.getHotfixLabel('')).toEqual('');
   });
 });
 
-describe("shouldUpdatePRDescription()", () => {
-  it("should return false when the hidden marker is present", () => {
+describe('shouldUpdatePRDescription()', () => {
+  it('should return false when the hidden marker is present', () => {
     expect(GitHub.shouldUpdatePRDescription(HIDDEN_MARKER)).toBeFalsy();
     expect(
       GitHub.shouldUpdatePRDescription(`
@@ -139,15 +97,14 @@ describe("shouldUpdatePRDescription()", () => {
 -->
 
 some actual content'
-    `),
+    `)
     ).toBeFalsy();
   });
 
-  it("should return true when the hidden marker is NOT present", () => {
-    expect(GitHub.shouldUpdatePRDescription("")).toBeTruthy();
-    expect(GitHub.shouldUpdatePRDescription("added_by")).toBeTruthy();
-    expect(GitHub.shouldUpdatePRDescription("added_by_something_else"))
-      .toBeTruthy();
+  it('should return true when the hidden marker is NOT present', () => {
+    expect(GitHub.shouldUpdatePRDescription('')).toBeTruthy();
+    expect(GitHub.shouldUpdatePRDescription('added_by')).toBeTruthy();
+    expect(GitHub.shouldUpdatePRDescription('added_by_something_else')).toBeTruthy();
     expect(
       GitHub.shouldUpdatePRDescription(`
 ## Checklist
@@ -156,28 +113,28 @@ some actual content'
 - [ ] All files are lint-free.
 - [ ] Added tests for the core-changes (as applicable).
 - [ ] Tested locally for regressions & all test cases are passing.
-`),
+`)
     ).toBeTruthy();
   });
 });
 
-describe("isHumongousPR()", () => {
-  it("should return true if additions are greater than the threshold", () => {
+describe('isHumongousPR()', () => {
+  it('should return true if additions are greater than the threshold', () => {
     expect(GitHub.isHumongousPR(2000, 500)).toBeTruthy();
   });
 
-  it("should return false if additions are less than the threshold", () => {
+  it('should return false if additions are less than the threshold', () => {
     expect(GitHub.isHumongousPR(200, 500)).toBeFalsy();
   });
 
-  it("should return false with erroneous inputs", () => {
+  it('should return false with erroneous inputs', () => {
     expect(GitHub.isHumongousPR(NaN, NaN)).toBeFalsy();
   });
 });
 
-describe("getHugePrComment()", () => {
-  it("should return the comment content with additions and threshold", () => {
-    expect(GitHub.getHugePrComment(1000, 800)).toContain("1000");
-    expect(GitHub.getHugePrComment(1000, 800)).toContain("800");
+describe('getHugePrComment()', () => {
+  it('should return the comment content with additions and threshold', () => {
+    expect(GitHub.getHugePrComment(1000, 800)).toContain('1000');
+    expect(GitHub.getHugePrComment(1000, 800)).toContain('800');
   });
 });
